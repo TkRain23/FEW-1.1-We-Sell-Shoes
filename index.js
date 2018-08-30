11eeef2516c7a1bd90eef20d4a951f573fc23ee6
tkrain23 <tk.rain09@gmail.com>
2018-08-30 13:17:05 -0700

    added faq section

--- index.js
+++ index.js
@@ -85,3 +85,31 @@ prev.addEventListener("click", function() {
 })
 
 ////////////////// end of testimonial section
+
+//delcare default vars
+const acc = document.getElementsByClassName("accordion");
+
+//loop through all accordion buttons
+for (let i = 0; i < acc.length; i++) {
+
+  //trigger an event after click
+  acc[i].addEventListener("click", function() {
+
+      //add in the active class
+      this.classList.toggle("active");
+
+    //select sibling element
+    let panel = this.nextElementSibling;
+
+    //if sibling is open, close it, if closed, open it
+    if (panel.style.maxHeight){
+      //panel is open
+      panel.style.maxHeight = null; //add this line
+
+    } else {
+      //panel is closed
+      panel.style.maxHeight = panel.scrollHeight + "px"; //add this line
+
+    }
+  });
+}
